f6ef2a1fc40b0c6a8d0b321444e304e1f0d6fbb3
Nort4 <60520357+Nort4@users.noreply.github.com>
2020-05-08 09:51:05 +0300

    installer.py

--- installer.py
+++ installer.py
@@ -74,9 +74,9 @@ elif number == '11':
 vnc()
 
 vnc = input("Введите номер дистрибутива : ")
-	if vnc == '1':
-		os.system("pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh")
-		os.system("./start-ubuntu.sh")
-		os.system("wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/DesktopEnvironment/Apt/Xfce4/de-apt-xfce4.sh && bash de-apt-xfce4.sh")
+if vnc == '1':
+	os.system("pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh")
+	os.system("./start-ubuntu.sh")
+	os.system("wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/DesktopEnvironment/Apt/Xfce4/de-apt-xfce4.sh && bash de-apt-xfce4.sh")
 else:
 	print("Проблемка! ")
